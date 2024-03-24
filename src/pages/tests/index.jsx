11ff98e4e767541a766529d71f988dc6508ff8ea
Ayubiy1 <ayubiyurinboyev@gmail.com>
2024-03-24 08:08:31 +0500

    fix: bulid

--- src/pages/tests/index.jsx
+++ src/pages/tests/index.jsx
@@ -5,7 +5,7 @@ import { FaRegHeart } from "react-icons/fa";
 
 import "./style.css";
 import { useNavigate } from "react-router";
-// import axios from "axios";
+import axios from "axios";
 
 const Tests = () => {
   const navigate = useNavigate();
@@ -16,7 +16,7 @@ const Tests = () => {
 
   return (
     <>
-      {/* <Row className={"flex items-start  justify-center  p-2"}>
+      <Row className={"flex items-start  justify-center  p-2"}>
         {isLoading == false &&
           data?.data?.map((item) => {
             return (
@@ -214,7 +214,7 @@ const Tests = () => {
             </Col>
           </>
         )}
-      </Row> */}
+      </Row>
     </>
   );
 };
